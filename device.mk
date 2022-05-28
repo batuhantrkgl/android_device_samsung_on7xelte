c0a315c0f9b05fadcd21a422a6753e3b4d4bc388
Flominator <github.flominatorgd@gmail.com>
2022-05-28 03:54:17 +0200

    on7xelte: add fingerprint permission to device speciffic

--- device.mk
+++ device.mk
@@ -47,6 +47,9 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.samsung
+   
+PRODUCT_COPY_FILES += \  
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
