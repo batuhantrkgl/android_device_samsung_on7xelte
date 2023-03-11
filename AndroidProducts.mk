samsungexynos7870/android_device_samsung_a3y17lte

declare use of oss audio and uprev to android.hardware.soundtrigger@2.3-impl, android.hardware.audio.effect@6.0-impl, android.hardware.audio@6.0-impl, add device speciffic wifi overlay, add fix for hotspot on android 11 for qcom wifi by setting WIFI_AVOID_IFACE_RESET_MAC_CHANGE := true, add missing android.hardware.nfc.uicc.xml, Add parameters for Hotspot 2.0 in wpa_supplicant_overlay.conf, refractor for arrow

AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019 The havocOS Project
+# Copyright (C) 2019 The arrowOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_a3y17lte.mk
+    $(LOCAL_DIR)/arrow_a3y17lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_a3y17lte-user \
-    havoc_a3y17lte-userdebug \
-    havoc_a3y17lte-eng
+    arrow_a3y17lte-user \
+    arrow_a3y17lte-userdebug \
+    arrow_a3y17lte-eng

device.mk
@@ -20,12 +20,8 @@ DEVICE_PATH := device/samsung/a3y17lte
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml
-
-# Custom mixer_paths
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
-    $(DEVICE_PATH)/configs/audio/mixer_gains.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_gains.xml
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
+		frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml
 
 # Launch Android API level
 PRODUCT_SHIPPING_API_LEVEL := 23
@@ -49,8 +45,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.samsung
-   
-PRODUCT_COPY_FILES += \  
+
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # NFC
@@ -90,6 +86,7 @@ PRODUCT_PACKAGES += \
     libcld80211 \
     libwifi-hal \
     libwpa_client \
+		WifiOverlay \
     wificond \
     wpa_supplicant \
     wpa_supplicant.conf
@@ -101,10 +98,27 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(DEVICE_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
+# Custom mixer_paths OSS
+PRODUCT_COPY_FILES += \
+$(LOCAL_PATH)/configs/audio/oss/mixer_paths_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0.xml
+
+# Inherit from common (audio)
+$(call inherit-product, device/samsung/universal7870-common/device-oss_audio.mk)
+
+# Custom mixer_paths prebuilt
+#PRODUCT_COPY_FILES += \
+#    $(DEVICE_PATH)/configs/prebuilt/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
+#    $(DEVICE_PATH)/configs/prebuilt/audio/mixer_gains.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_gains.xml
+
+# Inherit from common
+# $(call inherit-product, device/samsung/universal7870-common/device-prebuilt_audio.mk)
+
 # Properties
 -include $(DEVICE_PATH)/system_prop.mk
 
 # Inherit from common
 $(call inherit-product, device/samsung/universal7870-common/device-common.mk)
+
+# $(call inherit-product, device/samsung/universal7870-common/device-prebuilt_bsp-vndk.mk)
 
 $(call inherit-product-if-exists, vendor/samsung/a3y17lte/a3y17lte-vendor.mk)

batuhantrkgl/android_device_samsung_on7xelte

AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019 The havocOS Project
+# Copyright (C) 2019 The arrowOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_on7xelte.mk
+    $(LOCAL_DIR)/arrow_on7xelte.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_on7xelte-user \
-    havoc_on7xelte-userdebug \
-    havoc_on7xelte-eng
+    arrow_on7xelte-user \
+    arrow_on7xelte-userdebug \
+    arrow_on7xelte-eng

device.mk
@@ -18,12 +18,10 @@ DEVICE_PATH := device/samsung/on7xelte
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
-
-# Custom mixer_paths
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/audio/mixer_gains.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_gains.xml \
-    $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml
 
 # Launch Android API level
 PRODUCT_SHIPPING_API_LEVEL := 23
@@ -47,8 +45,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.samsung
-   
-PRODUCT_COPY_FILES += \  
+
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # Overlays
@@ -79,6 +77,7 @@ PRODUCT_PACKAGES += \
     hostapd \
     libwifi-hal \
     libwpa_client \
+		WifiOverlay \
     wificond \
     wifiloader \
     wifilogd \
@@ -90,10 +89,27 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(DEVICE_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
+# Custom mixer_paths OSS
+#PRODUCT_COPY_FILES += \
+#$(LOCAL_PATH)/configs/audio/oss/mixer_paths_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0.xml
+
+# Inherit from common (audio)
+#$(call inherit-product, device/samsung/universal7870-common/device-oss_audio.mk)
+
+# Custom mixer_paths prebuilt
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/prebuilt/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
+    $(DEVICE_PATH)/configs/prebuilt/audio/mixer_gains.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_gains.xml
+
+# Inherit from common
+ $(call inherit-product, device/samsung/universal7870-common/device-prebuilt_audio.mk)
+
 # Properties
 -include $(DEVICE_PATH)/system_prop.mk
 
 # Inherit from common
 $(call inherit-product, device/samsung/universal7870-common/device-common.mk)
+
+# $(call inherit-product, device/samsung/universal7870-common/device-prebuilt_bsp-vndk.mk)
 
 $(call inherit-product-if-exists, vendor/samsung/on7xelte/on7xelte-vendor.mk)